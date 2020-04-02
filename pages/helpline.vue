58180ef320d9145ac2714af1252182a06531c279
Stanley Masinde <stanleyloren@gmail.com>
2020-04-02 20:16:14 +0300

    📞 Helpline: Helpline contact information
    Signed-off-by: Stanley Masinde <stanleyloren@gmail.com>

--- pages/helpline.vue
+++ pages/helpline.vue
@@ -1,8 +1,33 @@
 <template>
   <v-row>
     <v-col>
-      <v-btn href="tel:/719">Call 719</v-btn> for Assistance. More information
-      can be found at <a href="http://health.go.ke">MOH Website</a>
+      <p class="title">Reporting illness</p>
+      If you feel unwell, have a <u>cold</u> or a <u>cough</u> or fever, please
+      call <a href="tel:719">719</a> or text <a href="tel:*719#">*719#</a> which
+      is a toll-free number provided by the Government of Kenya.
+      <v-list flat>
+        <v-subheader>
+          You can also call the following County Hotline numbers:
+        </v-subheader>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> Nairobi </v-list-item-title> (tollfree) /
+            0732353535</v-list-item-content
+          ></v-list-item
+        >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> Garissa</v-list-item-title> 0111207207 /
+            0110040836 / 011040708</v-list-item-content
+          ></v-list-item
+        >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> Turkana</v-list-item-title>
+            0758722023</v-list-item-content
+          ></v-list-item
+        >
+      </v-list>
     </v-col>
   </v-row>
 </template>
